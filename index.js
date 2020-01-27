3819bcb0049c92a0c8c9b2dba44aff8f7a70ebc8
joamag <joamag@gmail.com>
2020-01-27 16:15:21 +0000

    New index values

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-import { AppRegistry } from "react-native";
-import App from "./app";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => App);
+export * from "./react";
