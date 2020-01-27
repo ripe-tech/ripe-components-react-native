eef6ff5b39be80f95f82f2b1b6a62dd1c6e35f94
joamag <joamag@gmail.com>
2020-01-27 16:14:40 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    extends: "@react-native-community",
+    rules: {
+        quotes: [
+            "error",
+            "double",
+            {
+                avoidEscape: true
+            }
+        ],
+        semi: ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        "space-before-function-paren": [
+            "error",
+            {
+                anonymous: "never",
+                named: "never",
+                asyncArrow: "always"
+            }
+        ],
+        "no-alert": "off",
+        "react-native/no-inline-styles": "off"
+    }
+};
---
+++ react/components/molecules/button/button.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export class Button extends Component {
+    render() {
+        return <Text>This is a button</Text>;
+    }
+}
