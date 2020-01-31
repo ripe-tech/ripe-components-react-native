ce56ca1215978c21418a303a7a1f4b1547cd3920
Gabriel Candal <gcc@platforme.com>
2020-01-31 08:59:16 +0000

    Add button story

---
+++ react/components/molecules/button/button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { storiesOf } from '@storybook/react-native';
+
+import { Button } from "./button";
+
+storiesOf("Molecules", module)
+  .add("Button", () => (
+    <Button></Button>
+  ));
