452510331eeaa876dabcaafbdd5d1b1cff604ee6
Gabriel Candal <gcc@platforme.com>
2020-01-31 09:11:23 +0000

    Prettier

--- react/components/molecules/button/button.stories.js
+++ react/components/molecules/button/button.stories.js
@@ -3,7 +3,4 @@ import { storiesOf } from "@storybook/react-native";
 
 import { Button } from "./button";
 
-storiesOf("Molecules", module)
-  .add("Button", () => (
-    <Button></Button>
-  ));
+storiesOf("Molecules", module).add("Button", () => <Button></Button>);
