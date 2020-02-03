f0eb2917a15d13f71148f02d3c7d40753e0ce112
Gabriel Candal <gcc@platforme.com>
2020-02-03 09:45:48 +0000

    Small enhancements to button

--- react/components/atoms/button/button.js
+++ react/components/atoms/button/button.js
@@ -4,18 +4,18 @@ import LinearGradient from "react-native-linear-gradient";
 import PropTypes from "prop-types";
 
 export class Button extends PureComponent {
-    _gradientStart = { x: 0, y: 0 };
-
-    _gradientEnd = { x: 1, y: 0 };
-
-    _gradientColors = ["#4a6fe9", "#6687f6"];
-
-    _onPress = () => this.props.onPress();
-
     _rootStyle = () => {
         const { containerStyle } = this.props;
         return [styles.root, containerStyle];
     };
+
+    _gradientStart = this.props.gradientStart || { x: 0, y: 0 };
+
+    _gradientEnd = this.props.gradienteEnd || { x: 0, y: 0 };
+
+    _gradientColors = this.props.gradientColors || ["#4a6fe9", "#6687f6"];
+
+    _onPress = () => this.props.onPress();
 
     render() {
         const { icon, text } = this.props;
@@ -43,8 +43,7 @@ export class Button extends PureComponent {
 const styles = StyleSheet.create({
     root: {
         borderRadius: 6,
-        overflow: "hidden",
-        minHeight: 48
+        height: 48
     },
     container: {
         flex: 1,
@@ -57,18 +56,19 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 16,
-        fontWeight: "normal",
-        fontStyle: "normal",
         lineHeight: 20,
-        letterSpacing: 0.34,
+        letterSpacing: 0.5,
         textAlign: "center",
-        color: "white"
+        color: "#ffffff"
     }
 });
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
     icon: PropTypes.number,
-    onPress: PropTypes.func.isRequired,
-    containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+    containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    gradientColors: PropTypes.arrayOf(PropTypes.string),
+    gradientStart: PropTypes.shape({ x: PropTypes.number, y: PropTypes.number }),
+    gradientEnd: PropTypes.shape({ x: PropTypes.number, y: PropTypes.number }),
+    onPress: PropTypes.func.isRequired
 };
