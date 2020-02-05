1ab6039d0f3acaaa0114f53e562a5c54eacb3aff
Gabriel Candal <gcc@platforme.com>
2020-02-05 17:45:48 +0000

    Add tag

--- react/components/atoms/index.js
+++ react/components/atoms/index.js
@@ -1,4 +1,5 @@
 export * from "./button";
 export * from "./button-keyboard";
 export * from "./button-tab";
+export * from "./tag";
 export * from "./icon";
---
+++ react/components/atoms/tag/tag.js
@@ -0,0 +1,80 @@
+import React, { PureComponent } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Icon } from "../icon";
+
+import PropTypes from "prop-types";
+
+export class Tag extends PureComponent {
+    static get propTypes() {
+        return {
+            color: PropTypes.string,
+            backgroundColor: PropTypes.string,
+            borderColor: PropTypes.string,
+            text: PropTypes.string,
+            icon: PropTypes.string,
+            iconWidth: PropTypes.number,
+            iconHeight: PropTypes.number,
+            onPress: PropTypes.func
+        };
+    }
+
+    static get defaultProps() {
+        return {
+            color: "#57626e",
+            backgroundColor: "#eceef1",
+            borderColor: "red",
+            text: undefined,
+            icon: undefined,
+            iconWidth: undefined,
+            iconHeight: undefined,
+            onPress: () => {}
+        };
+    }
+
+    _style = () => {
+        return Object.assign({}, styles.style, {
+            backgroundColor: this.props.backgroundColor,
+            borderColor: this.props.borderColor
+        });
+    };
+
+    _textStyle = () => {
+        return Object.assign({}, styles.text, {
+            color: this.props.color,
+            marginLeft: this.props.icon ? 8 : undefined
+        });
+    };
+
+    render() {
+        return (
+            <TouchableOpacity style={this._style()} onPress={this.props.onPress}>
+                {this.props.icon ? (
+                    <Icon
+                        icon={this.props.icon}
+                        color={this.props.color}
+                        width={this.props.iconWidth}
+                        height={this.props.iconHeight}
+                    />
+                ) : null}
+                {this.props.text ? <Text style={this._textStyle()}>{this.props.text}</Text> : null}
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    style: {
+        alignSelf: "flex-start",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        borderRadius: 6,
+        borderColor: "red",
+        borderWidth: 1
+    },
+    text: {
+        fontSize: 14
+    }
+});
