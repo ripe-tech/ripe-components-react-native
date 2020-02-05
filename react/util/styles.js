535418fccdca049ed257acc7c14531a21973dccd
joamag <joamag@gmail.com>
2020-02-05 20:09:10 +0000

    New font values

--- react/util/styles.js
+++ react/util/styles.js
@@ -1,8 +1,10 @@
-export const NORMAL_FONT = "Soleil-Light";
-export const NORMAL_ITALIC_FONT = "Soleil-Light-Italic";
-export const BOLD_FONT = "Soleil-Bold";
-export const BOLD_ITALIC_FONT = "Soleil-Bold-Italic";
+export const FONT_LIGHT = "Soleil-Light";
+export const FONT_LIGHT_ITALIC = "Soleil-Light-Italic";
+export const FONT_REGULAR = "Soleil-Regular";
+export const FONT_BOOK = "Soleil-Book";
+export const FONT_BOLD = "Soleil-Bold";
+export const FONT_BOLD_ITALIC = "Soleil-Bold-Italic";
+
+export const FONT = FONT_REGULAR;
 
 export const FONT_SIZE = 14;
-
-export const FONT = NORMAL_FONT;
