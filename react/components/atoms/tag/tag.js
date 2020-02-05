374f0be52f0fd056bf79f2dbea0dfefad1db5c80
joamag <joamag@gmail.com>
2020-02-05 21:14:18 +0000

    New paddding

--- react/components/atoms/tag/tag.js
+++ react/components/atoms/tag/tag.js
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         paddingVertical: 6,
-        paddingHorizontal: 8,
+        paddingHorizontal: 12,
         borderRadius: 6,
         borderColor: "red",
         borderWidth: 0
