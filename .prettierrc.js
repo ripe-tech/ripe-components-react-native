ede625b547d5ae1a7a62fbf981497cb94c6e582f
Gabriel Candal <gcc@platforme.com>
2020-02-11 16:12:22 +0000

    Add pretty

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,1 @@
-module.exports = {
-    printWidth: 100,
-    trailingComma: "none",
-    tabWidth: 4,
-    semi: true,
-    singleQuote: false
-};
+module.exports = require("prettier-config-hive");
