9c2c701f60d6a3f5d82e76c2c1a31a2812351dec
Gabriel Candal <gcc@platforme.com>
2020-02-12 13:06:24 +0000

    Fix problems with duplicated SafeAreaView

--- react/components/molecules/tabs/tabs.js
+++ react/components/molecules/tabs/tabs.js
@@ -1,10 +1,9 @@
 import React, { PureComponent } from "react";
 import { StyleSheet } from "react-native";
-
+import SafeAreaView from "react-native-safe-area-view";
 import PropTypes from "prop-types";
 
 import { ButtonTab } from "../../atoms/button-tab";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export class Tabs extends PureComponent {
     static get propTypes() {
