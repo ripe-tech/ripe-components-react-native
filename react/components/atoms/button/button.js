1461bb58162643bff9549bd8621bde5da33381bf
Gabriel Candal <gcc@platforme.com>
2020-02-13 11:31:55 +0000

    Revert bad change

--- react/components/atoms/button/button.js
+++ react/components/atoms/button/button.js
@@ -31,7 +31,7 @@ export class Button extends PureComponent {
             gradientAngle: 62,
             gradientLocations: [0.4, 0.84],
             gradientColors: ["#4a6fe9", "#6687f6"],
-            width: "100%",
+            width: undefined,
             style: {},
             onPress: () => {}
         };
