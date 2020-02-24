6a590aaacdf45905587cb7cbfd05f17507f9ac80
Flavio Lima <limfr.flavio.work@gmail.com>
2020-02-24 10:55:24 +0000

    Creation of button icon

--- react/components/atoms/index.js
+++ react/components/atoms/index.js
@@ -1,6 +1,7 @@
 export * from "./avatar";
 export * from "./button";
 export * from "./button-keyboard";
+export * from "./button-icon";
 export * from "./button-tab";
 export * from "./key-value";
 export * from "./icon";
