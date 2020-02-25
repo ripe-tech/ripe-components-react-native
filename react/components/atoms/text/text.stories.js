b1585ffcc9808cef3944fb87b49312a6cf1db1dd
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-25 16:08:06 +0000

    Update text.stories.js

--- react/components/atoms/text/text.stories.js
+++ react/components/atoms/text/text.stories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 import { Text } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Text", () => {
-        return <Text />;
+        const value = text("Value", "Text example");
+        return <Text>{value}</Text>;
     });
