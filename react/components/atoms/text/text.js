3eabf114a2f51dd0e5a5ccd8813e82e48de3ba82
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-25 16:08:03 +0000

    Text with expected behaviour

--- react/components/atoms/text/text.js
+++ react/components/atoms/text/text.js
@@ -3,12 +3,7 @@ import { StyleSheet, Text as RNText } from "react-native";
 
 export class Text extends PureComponent {
     render() {
-        return (
-            <RNText style={styles.text}>
-                Experience is the teacher of all things is the teacher of all things, experience is
-                the teacher of teacher all things is the teacher of all things.
-            </RNText>
-        );
+        return <RNText style={styles.text}>{this.props.children}</RNText>;
     }
 }
 
