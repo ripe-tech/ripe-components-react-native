5439a4676e5f117fe6b20e2b9b8a39f7df25e078
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-25 17:23:26 +0000

    Component props

--- react/components/atoms/link/link.js
+++ react/components/atoms/link/link.js
@@ -1,9 +1,29 @@
 import React, { PureComponent } from "react";
 import { Text } from "react-native";
 
+import PropTypes from "prop-types";
+
 export class Link extends PureComponent {
+    static get propTypes() {
+        return {
+            text: PropTypes.string,
+            url: PropTypes.string.isRequired
+        };
+    }
+
+    static get defaultProps() {
+        return {
+            text: undefined,
+            url: undefined
+        };
+    }
+
+    teste = () => {
+        return `${this.props.text} - ${this.props.url}`;
+    };
+
     render() {
-        return <Text>Hi! I'm Link</Text>;
+        return <Text>{this.teste()}</Text>;
     }
 }
 
--- react/components/atoms/link/link.stories.js
+++ react/components/atoms/link/link.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text as knobText } from "@storybook/addon-knobs";
 
 import { Link } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Link", () => {
-        return <Link />;
+        const text = knobText("Text", "Platforme Link");
+        const url = knobText("Url", "https://platforme.com/");
+        return <Link text={text} url={url} />;
     });
