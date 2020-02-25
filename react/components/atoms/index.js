af7c8a2899da33b9614649351f818e529383c89e
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-25 17:09:36 +0000

    Initial commit

--- react/components/atoms/index.js
+++ react/components/atoms/index.js
@@ -3,5 +3,6 @@ export * from "./button";
 export * from "./button-keyboard";
 export * from "./button-tab";
 export * from "./key-value";
+export * from "./link";
 export * from "./icon";
 export * from "./tag";
---
+++ react/components/atoms/link/link.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import { Text } from "react-native";
+
+export class Link extends PureComponent {
+    render() {
+        return <Text>Hi! I'm Link</Text>;
+    }
+}
+
+export default Link;
