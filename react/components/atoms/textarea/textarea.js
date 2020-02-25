e123767e92f78038bcb033774137d5394b3bd88f
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-25 13:03:14 +0000

    Component props

--- react/components/atoms/textarea/textarea.js
+++ react/components/atoms/textarea/textarea.js
@@ -1,24 +1,45 @@
 import React, { PureComponent } from "react";
-import {Text} from "react-native";
+import { ViewPropTypes, Text } from "react-native";
 
 import PropTypes from "prop-types";
 
 export class TextArea extends PureComponent {
     static get propTypes() {
-        return {};
+        return {
+            value: PropTypes.string,
+            placeholder: PropTypes.string,
+            multiline: PropTypes.bool,
+            minHeight: PropTypes.oneOfType([
+                PropTypes.string,
+                PropTypes.number,
+            ]),
+            onValue: PropTypes.func,
+            style: ViewPropTypes.style
+        };
     }
 
     static get defaultProps() {
-        return {};
+        return {
+            value: undefined,
+            placeholder: undefined,
+            multiline: false,
+            minHeight: undefined,
+            onValue: () => {},
+            style: {}
+        };
     }
 
-    _imageStyles = () => {
+    test = () => {
+        return `${this.props.value}\n${this.props.placeholder}\n${this.props.multiline}\n${this.props.minHeight}`
+    }
+
+    _style = () => {
         return [];
     };
 
     render() {
         return (
-            <Text>Hello ! I'm textarea</Text>
+        <Text>{this.test()}</Text>
         );
     }
 }
--- react/components/atoms/textarea/textarea.stories.js
+++ react/components/atoms/textarea/textarea.stories.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text, boolean, number} from "@storybook/addon-knobs";
 
 import { TextArea } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Textarea", () => {
+        const value = text("Value", undefined);
+        const placeholder = text("Placeholder", "Placeholder example");
+        const multiline = boolean("Multiline", false);
+        const minHeight = number("Min Height", undefined);
         return (
-            <TextArea />
+            <TextArea
+                value={value}
+                placeholder={placeholder}
+                multiline={multiline}
+                minHeight={minHeight}
+            />
         );
     });
