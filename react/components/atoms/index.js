00447401075a6e4abefa4d6ec4d45a2cdc6506e8
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-26 10:59:47 +0000

    Reorder

--- react/components/atoms/index.js
+++ react/components/atoms/index.js
@@ -3,6 +3,6 @@ export * from "./button";
 export * from "./button-keyboard";
 export * from "./button-tab";
 export * from "./key-value";
-export * from "./link";
 export * from "./icon";
+export * from "./link";
 export * from "./tag";
