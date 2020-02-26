6eec829097247658452a0b17c32ddd4eb355317e
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-26 12:17:13 +0000

    Style

--- react/components/atoms/text/text.js
+++ react/components/atoms/text/text.js
@@ -1,19 +1,34 @@
 import React, { PureComponent } from "react";
-import { StyleSheet, Text as RNText } from "react-native";
+import { ViewPropTypes, Text as RNText } from "react-native";
 
 export class Text extends PureComponent {
+    static get propTypes() {
+        return {
+            style: ViewPropTypes.style
+        };
+    }
+
+    static get defaultProps() {
+        return {
+            style: {}
+        };
+    }
+
+    _style = () => {
+        return [
+            this.props.style,
+            {
+                fontFamily: "Soleil",
+                fontSize: 14,
+                lineHeight: 22,
+                color: "#3e566a"
+            }
+        ];
+    };
+
     render() {
-        return <RNText style={styles.text}>{this.props.children}</RNText>;
+        return <RNText style={this._style()}>{this.props.children}</RNText>;
     }
 }
-
-const styles = StyleSheet.create({
-    text: {
-        fontFamily: "Soleil",
-        fontSize: 14,
-        lineHeight: 22,
-        color: "#3e566a"
-    }
-});
 
 export default Text;
