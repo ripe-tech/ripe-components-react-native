58cf6785276b5329aa463d91a8b26a9bed1a2564
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-26 16:17:56 +0000

    Needed props

--- react/components/atoms/container/container.js
+++ react/components/atoms/container/container.js
@@ -1,23 +1,37 @@
 import React, { PureComponent } from "react";
-import { Text } from "react-native";
+import { ViewPropTypes, Text } from "react-native";
 
 import PropTypes from "prop-types";
 
 export class Container extends PureComponent {
     static get propTypes() {
-        return {};
+        return {
+            header: PropTypes.bool,
+            headerIcon: PropTypes.string,
+            headerDate: PropTypes.number,
+            style: ViewPropTypes.style
+        };
     }
 
     static get defaultProps() {
-        return {};
+        return {
+            header: false,
+            headerIcon: undefined,
+            headerDate: undefined,
+            style: {}
+        };
     }
 
     _style = () => {
         return [{}, this.props.style];
     };
 
+    propsTest = () => {
+        return `${this.props.header}\n${this.props.headerIcon}\n${this.props.headerDate}`;
+    };
+
     render() {
-        return <Text>Hi! I'm Container</Text>;
+        return <Text>{this.propsTest()}</Text>;
     }
 }
 
--- react/components/atoms/container/container.stories.js
+++ react/components/atoms/container/container.stories.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, boolean, text, number } from "@storybook/addon-knobs";
 
 import { Container } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Container", () => {
-        return <Container />;
+        const header = boolean("Header", false);
+        const headerIcon = text("Header Icon", "happy-face");
+        const headerDate = number("Header Date", 1337);
+        return <Container header={header} headerIcon={headerIcon} headerDate={headerDate} />;
     });
