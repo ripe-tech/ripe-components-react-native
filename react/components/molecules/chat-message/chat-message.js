38f9326310c2f8e19eaaa1a614fad1ef49771876
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-26 10:26:16 +0000

    Props

--- react/components/molecules/chat-message/chat-message.js
+++ react/components/molecules/chat-message/chat-message.js
@@ -5,15 +5,34 @@ import PropTypes from "prop-types";
 
 export class ChatMessage extends PureComponent {
     static get propTypes() {
-        return {};
+        return {
+            avatarUrl: PropTypes.string.isRequired,
+            username: PropTypes.string.isRequired,
+            message: PropTypes.string.isRequired,
+            attachments: PropTypes.arrayOf(
+                PropTypes.exact({
+                    name: PropTypes.string.isRequired,
+                    path: PropTypes.string.isRequired
+                })
+            )
+        };
     }
 
     static get defaultProps() {
-        return {};
+        return {
+            avatarUrl: undefined,
+            username: undefined,
+            message: undefined,
+            attachments: []
+        };
     }
 
+    propsTest = () => {
+        return `${this.props.avatarUrl}\n${this.props.username}\n${this.props.message}\n${this.props.attachments}`;
+    };
+
     render() {
-        return <Text>Hi! I'm ChatMessage</Text>;
+        return <Text>{this.propsTest()}</Text>;
     }
 }
 
--- react/components/molecules/chat-message/chat-message.stories.js
+++ react/components/molecules/chat-message/chat-message.stories.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 import { ChatMessage } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Link", () => {
-        return <ChatMessage />;
+        const avatarUrl = text(
+            "Avatar URL",
+            "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar"
+        );
+        const username = text("Username", "NFSS10");
+        const message = text(
+            "Message",
+            "Cras faucibus mauris sem, vel ultricies turpis venenatis et. Mauris sed tristique purus. Cras vulputate vitae velit eget hendrerit. Aliquam mattis commodo tellus. Sed ac ligula condimentum, euismod eros non, dignissim leo. Donec faucibus lectus non enim tincidunt, at laoreet neque auctor. Nam at arcu tortor. "
+        );
+        const attachments = [
+            {
+                name: "dummy.pdf",
+                path: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+            },
+            {
+                name: "image.png",
+                path: "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar"
+            }
+        ];
+        return (
+            <ChatMessage
+                avatarUrl={avatarUrl}
+                username={username}
+                message={message}
+                attachments={attachments}
+            />
+        );
     });
