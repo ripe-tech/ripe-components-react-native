ef97c2f6230f5c74fc569805478eecfe8966528e
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-27 09:35:35 +0000

    Props + Clickable Image

--- react/components/atoms/lightbox/lightbox.js
+++ react/components/atoms/lightbox/lightbox.js
@@ -1,30 +1,49 @@
 import React, { PureComponent } from "react";
-import { ViewPropTypes, Text } from "react-native";
+import { ViewPropTypes, TouchableOpacity, Image } from "react-native";
 
 import PropTypes from "prop-types";
 
 export class Lightbox extends PureComponent {
     static get propTypes() {
         return {
+            src: PropTypes.string.isRequired,
+            width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+            height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+            borderRadius: PropTypes.number,
             style: ViewPropTypes.style
         };
     }
 
     static get defaultProps() {
         return {
-            style: {}
+            src: undefined,
+            width: "100%",
+            height: "100%",
+            borderRadius: undefined
         };
     }
 
+    onLightboxPress = () => {
+        console.log("sup");
+    };
+
     _style = () => {
-        return [
-            this.props.style
-        ];
+        return [this.props.style];
+    };
+
+    _imageStyle = () => {
+        return {
+            width: this.props.width,
+            height: this.props.height,
+            borderRadius: this.props.borderRadius
+        };
     };
 
     render() {
         return (
-            <Text>Hi ! I'm Lightbox</Text>
+            <TouchableOpacity onPress={() => this.onLightboxPress()}>
+                <Image style={this._imageStyle()} source={{ uri: this.props.src }} />
+            </TouchableOpacity>
         );
     }
 }
--- react/components/atoms/lightbox/lightbox.stories.js
+++ react/components/atoms/lightbox/lightbox.stories.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text, number } from "@storybook/addon-knobs";
 
 import { Lightbox } from "../../";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Lightbox", () => {
-        return (
-            <Lightbox />
+        const src = text(
+            "Image Source",
+            "https://id.platforme.com/admin/accounts/t-ms%40platforme.com/avatar"
         );
+        const width = number("Width", 200);
+        const height = number("Height", 150);
+        const borderRadius = number("Border Radius", undefined);
+        return <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />;
     });
