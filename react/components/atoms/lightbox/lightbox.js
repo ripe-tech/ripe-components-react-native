01dd25611cb20d267961c7226f11f18017c8b3ec
Nuno Silva <nuno_nfss96@hotmail.com>
2020-02-28 09:12:38 +0000

    Fixed translate

--- react/components/atoms/lightbox/lightbox.js
+++ react/components/atoms/lightbox/lightbox.js
@@ -39,12 +39,13 @@ export class Lightbox extends PureComponent {
             height: this.props.height,
             borderRadius: this.props.borderRadius,
             fadeAnimationValue: new Animated.Value(0),
-            widthAnimationValue: new Animated.Value(1),
-            heightAnimationValue: new Animated.Value(1),
+            widthAnimationValue: new Animated.Value(0),
+            heightAnimationValue: new Animated.Value(0),
             translateXAnimationValue: new Animated.Value(0),
             translateYAnimationValue: new Animated.Value(0)
         };
 
+        this.posX = this.posY = undefined;
         this.screenWidth = Dimensions.get("window").width;
         this.screenHeight = Dimensions.get("window").height;
     }
@@ -55,13 +56,13 @@ export class Lightbox extends PureComponent {
         });
     };
 
+    _onLayout = event => {
+        this.posX = event.nativeEvent.layout.x;
+        this.posY = event.nativeEvent.layout.y;
+    };
+
     startAnimation = () => {
         const openAnimationTime = 5000;
-
-        const scale = this.screenWidth / this.props.width;
-
-        const finalPosX = this.screenWidth / 4;
-        const finalPosY = this.props.height * scale;
 
         Animated.parallel([
             Animated.timing(this.state.fadeAnimationValue, {
@@ -69,19 +70,19 @@ export class Lightbox extends PureComponent {
                 duration: 200
             }),
             Animated.timing(this.state.widthAnimationValue, {
-                toValue: scale,
+                toValue: 1,
                 duration: openAnimationTime
             }),
             Animated.timing(this.state.heightAnimationValue, {
-                toValue: scale,
+                toValue: 1,
                 duration: openAnimationTime
             }),
             Animated.timing(this.state.translateXAnimationValue, {
-                toValue: finalPosX,
+                toValue: 1,
                 duration: openAnimationTime
             }),
             Animated.timing(this.state.translateYAnimationValue, {
-                toValue: finalPosY,
+                toValue: 1,
                 duration: openAnimationTime
             })
         ]).start();
@@ -90,12 +91,22 @@ export class Lightbox extends PureComponent {
     _translateStyle = () => {
         this.startAnimation();
 
+        const scale = this.screenWidth / this.props.width;
+        const finalPosX = (this.screenWidth / 4);
+        const finalPosY = (this.screenHeight)/2 - (this.props.height*scale)/2
+
         return {
-            width: this.screenWidth/2,
-            height: this.screenWidth/2,
+            width: this.screenWidth / 2,
+            height: this.screenWidth / 2,
             transform: [
-                { translateX: this.state.translateXAnimationValue },
-                { translateY: this.state.translateYAnimationValue }
+                { translateX: this.state.translateXAnimationValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [this.posX, finalPosX]
+                }) },
+                { translateY: this.state.translateYAnimationValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [this.posY, finalPosY]
+                }) }
             ]
         };
     };
@@ -113,6 +124,8 @@ export class Lightbox extends PureComponent {
         /*         const finalWidth = this.screenWidth;
         this.props.width */
 
+        const scale = this.screenWidth / this.props.width;
+
         return {
             flex: 1,
             alignSelf: "center",
@@ -120,8 +133,18 @@ export class Lightbox extends PureComponent {
             //height: this.props.height,
             resizeMode: "contain",
             transform: [
-                { scaleX: this.state.widthAnimationValue },
-                { scaleY: this.state.heightAnimationValue }
+                {
+                    scaleX: this.state.widthAnimationValue.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [1, scale]
+                    })
+                },
+                {
+                    scaleY: this.state.heightAnimationValue.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [1, scale]
+                    })
+                }
             ]
         };
     };
@@ -162,14 +185,16 @@ export class Lightbox extends PureComponent {
         };
 
         return (
-            <Animated.View style={[this._fullscreenStyle(), this.props.style]}>
+            <Animated.View
+                style={[this._fullscreenStyle(), this.props.style]}
+                onLayout={event => this._onLayout(event)}
+            >
                 {this.state.visible ? fullscreenImage() : pressableImage()}
             </Animated.View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default Lightbox;
--- react/components/atoms/lightbox/lightbox.stories.js
+++ react/components/atoms/lightbox/lightbox.stories.js
@@ -2,6 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { withKnobs, text, number } from "@storybook/addon-knobs";
 
+import { View } from "react-native";
+
 import { Lightbox } from "../../";
 
 storiesOf("Atoms", module)
@@ -9,10 +11,19 @@ storiesOf("Atoms", module)
     .add("Lightbox", () => {
         const src = text(
             "Image Source",
-            "https://id.platforme.com/admin/accounts/t-ms%40platforme.com/avatar"
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2F4jkwjjrlxnr21.jpg&f=1&nofb=1"
         );
         const width = number("Width", 200);
         const height = number("Height", 150);
         const borderRadius = number("Border Radius", undefined);
-        return <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />;
+        return (
+            <View style={{flex: 1,
+                flexDirection: 'row',
+                flexWrap: 'wrap'}}>
+                <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />
+                <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />
+                <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />
+                <Lightbox src={src} width={width} height={height} borderRadius={borderRadius} />
+            </View>
+        );
     });
