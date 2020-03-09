8bbbb447b4fa8c195bfbc9d8606b06aa18c22bba
Nuno Silva <nuno_nfss96@hotmail.com>
2020-03-09 12:20:24 +0000

    Updated stories

--- react/components/organisms/chat/chat.stories.js
+++ react/components/organisms/chat/chat.stories.js
@@ -2,15 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { withKnobs, text } from "@storybook/addon-knobs";
 
-import { Chat } from "../../";
+import { Chat } from "../..";
 
 storiesOf("Organisms", module)
     .addDecorator(withKnobs)
     .add("Chat", () => {
-        const onNewMessage = message => {
-            messages.push(message);
-            console.log("New Message", message);
-        };
         const avatarUrl = text(
             "Avatar URL",
             "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar"
@@ -71,23 +67,50 @@ storiesOf("Organisms", module)
                 attachments: []
             },
             {
-                avatarUrl: "https://id.platforme.com/admin/accounts/v-fl%40platforme.com/avatar",
-                username: "FlaviooLima",
-                message: "Lorem ipsum dolor sit.",
+                avatarUrl: "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar",
+                username: "NFSS10",
+                message:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                date: 1574950742823,
+                attachments: [
+                    {
+                        name: "dummy.pdf",
+                        path:
+                            "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                    }
+                ]
+            },
+            {
+                avatarUrl: "https://id.platforme.com/admin/accounts/t-ms%40platforme.com/avatar",
+                username: "BeeMargarida",
+                message:
+                    "Praesent a lectus non libero facilisis tincidunt ut.",
                 date: 1574950742823,
                 attachments: []
             },
             {
-                avatarUrl: "https://id.platforme.com/admin/accounts/v-fl%40platforme.com/avatar",
-                username: "FlaviooLima",
-                message: "Lorem ipsum dolor sit.",
+                avatarUrl: "https://id.platforme.com/admin/accounts/v-da%40platforme.com/avatar",
+                username: "3rdvision",
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                date: 1574950742823,
+                attachments: [
+                    {
+                        name: "dummy.jpeg",
+                        path: "https://sandbox.platforme.com/api/compose?brand=dummy&model=dummy"
+                    }
+                ]
+            },
+            {
+                avatarUrl: "https://id.platforme.com/admin/accounts/ns%40platforme.com/avatar",
+                username: "NFSS10",
+                message: "Sed tristique faucibus interdum. Cras egestas efficitur sem id mollis. Cras varius malesuada orci, in vulputate urna vulputate a.",
                 date: 1574950742823,
                 attachments: []
             },
             {
-                avatarUrl: "https://id.platforme.com/admin/accounts/v-fl%40platforme.com/avatar",
-                username: "FlaviooLima",
-                message: "Lorem ipsum dolor sit.",
+                avatarUrl: "https://id.platforme.com/admin/accounts/t-ms%40platforme.com/avatar",
+                username: "BeeMargarida",
+                message: "Donec varius eget eros a euismod.",
                 date: 1574950742823,
                 attachments: []
             }
@@ -97,7 +120,6 @@ storiesOf("Organisms", module)
                 avatarUrl={avatarUrl}
                 username={username}
                 messages={messages}
-                onNewMessage={onNewMessage}
             />
         );
     });
