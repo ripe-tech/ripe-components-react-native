f0c6da9dbad1d646d17d0ee0c8ef48d786005837
Nuno Silva <nuno_nfss96@hotmail.com>
2020-03-17 17:38:58 +0000

    Removed imported PureComponent that was never used

--- react/components/organisms/image-list/image-list.stories.js
+++ react/components/organisms/image-list/image-list.stories.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { storiesOf } from "@storybook/react-native";
 import { withKnobs } from "@storybook/addon-knobs";
 
