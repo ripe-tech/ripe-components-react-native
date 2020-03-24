b0a58f45cf1a366d2aa33a74222511fb9c4da15b
Nuno Silva <nuno_nfss96@hotmail.com>
2020-03-24 15:57:15 +0000

    Using text instead of color for storybook story

--- react/components/molecules/key-value/key-value.stories.js
+++ react/components/molecules/key-value/key-value.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs, text, select, number, color } from "@storybook/addon-knobs";
+import { withKnobs, text, select, number } from "@storybook/addon-knobs";
 
 import { KeyValue } from "./key-value";
 
@@ -9,15 +9,15 @@ storiesOf("Atoms", module)
     .add("Key Value", () => {
         const key = text("Key", "Key");
         const value = text("Value", "Value");
-        const keyColor = color("Key Color", "#4f7af8");
-        const valueColor = color("Value Color", "#223645");
+        const keyColor = text("Key Color", "#4f7af8");
+        const valueColor = text("Value Color", "#223645");
         const icon = select(
             "Icon",
             { Add: "add", Alarm: "alarm", Bell: "bell", Phone: "phone", None: null },
             null
         );
-        const iconBackgroundColor = color("Icon Background Color", "#000000");
-        const iconColor = color("Icon Color", "#ffffff");
+        const iconBackgroundColor = text("Icon Background Color", "#000000");
+        const iconColor = text("Icon Color", "#ffffff");
         const iconSize = number("Icon Size", 30);
         const iconHeight = number("Icon Height", 20);
         const iconWidth = number("Icon Width", 20);
