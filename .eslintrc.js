50c2a3d681b0f2fe6cc0d7245778505a76f757b5
joamag <joamag@hive.pt>
2020-04-01 10:44:58 +0100

    Removed rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,6 @@ module.exports = {
         ],
         semi: ["error", "always"],
         "comma-dangle": ["error", "never"],
-        "space-before-function-paren": [
-            "error",
-            {
-                anonymous: "never",
-                named: "never",
-                asyncArrow: "always"
-            }
-        ],
         "no-alert": "off",
         "react-native/no-inline-styles": "off",
         "react/no-did-mount-set-state": "off",
