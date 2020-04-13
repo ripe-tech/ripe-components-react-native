448eb4855e995e004b60b13997eb3f209bd43308
FlaviooLima <v-fl@platforme.com>
2020-04-13 12:32:44 +0100

    Fix colours

--- react/components/molecules/key-value-placeholder/key-value-placeholder.stories.js
+++ react/components/molecules/key-value-placeholder/key-value-placeholder.stories.js
@@ -8,7 +8,7 @@ storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Key Value Placeholder", () => {
         const numberOfLines = number("Number of lines", 1);
-        const placeholderColors = ["#DBDEE5", "#E3E6ED", "#DBDEE5"];
+        const placeholderColors = ["#dbdee5", "#e3e6ed", "#dbdee5"];
         const placeholderLocations = [0, 0.3, 0.4];
         return (
             <KeyValuePlaceholder
