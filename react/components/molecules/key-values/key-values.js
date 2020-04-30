d3901105935ff221736d06e8a124ecb8b51c3ae6
Nuno Silva <nuno_nfss96@hotmail.com>
2020-04-30 13:54:43 +0100

    Supporting only two columns

--- react/components/molecules/key-values/key-values.js
+++ react/components/molecules/key-values/key-values.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 
 import { KeyValue } from "../key-value";
+import { isTabletSize } from "../../../util/ui-utils";
 
 export class KeyValues extends PureComponent {
     static get propTypes() {
@@ -26,65 +27,24 @@ export class KeyValues extends PureComponent {
                     onLongPress: PropTypes.func
                 })
             ).isRequired,
-            columnsNr: PropTypes.number,
+            twoColumns: PropTypes.bool,
             style: ViewPropTypes.style
         };
     }
 
     static get defaultProps() {
         return {
-            columnsNr: undefined,
+            twoColumns: isTabletSize(),
             style: {}
         };
     }
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            keyValuesFiller: this.props.columnsNr
-                ? this.props.items.length % this.props.columnsNr
-                : 0
-        };
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.columnsNr !== this.props.columnsNr) {
-            this.setState({
-                keyValuesFiller: this.props.columnsNr
-            });
-        }
-    }
-
-    width = () => `${(100 / this.props.columnsNr) * 0.96}%`;
-
     _style = () => {
-        return [this.props.columnsNr ? styles.keyValuesColumns : {}, this.props.style];
+        return [this.props.twoColumns ? styles.keyValuesColumns : {}, this.props.style];
     };
 
-    _keyValueStyle = (isFiller = false) => {
-        return this.props.columnsNr
-            ? {
-                  width: this.width(),
-                  opacity: isFiller ? 0 : 1
-              }
-            : {};
-    };
-
-    _keyValuesFiller = () => {
-        const filler = [];
-
-        for (let i = 0; i < this.state.keyValuesFiller; i++)
-            filler.push(
-                <KeyValue
-                    style={this._keyValueStyle(true)}
-                    key={`fill-${i}`}
-                    _key={`fill-${i}`}
-                    value={""}
-                />
-            );
-
-        return filler;
+    _keyValueStyle = () => {
+        return this.props.twoColumns ? styles.keyValueTwoColumns : {};
     };
 
     render() {
@@ -113,7 +73,6 @@ export class KeyValues extends PureComponent {
                         {item.valueComponent}
                     </KeyValue>
                 ))}
-                {this._keyValuesFiller()}
             </View>
         );
     }
@@ -124,6 +83,9 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         flexWrap: "wrap",
         justifyContent: "space-between"
+    },
+    keyValueTwoColumns: {
+        width: "48%"
     }
 });
 
