ec264c3401deb6718afe3c39c94cf271a64b9f00
Mauro Benedito <jorge.b.mauro@gmail.com>
2021-05-11 10:49:24 +0100

    Revert "style: pretty"
    This reverts commit 7756d3184deb2110260a71994ab0129f9b89226b.

--- react/components/organisms/chat/chat.stories.js
+++ react/components/organisms/chat/chat.stories.js
@@ -23,11 +23,13 @@ storiesOf("Components/Organisms/Chat", module)
                 attachments: [
                     {
                         name: "lorem-ipsum.pdf",
-                        path: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                        path:
+                            "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
                     },
                     {
                         name: "lorem-ipsum2.pdf",
-                        path: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                        path:
+                            "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
                     }
                 ]
             },
@@ -73,7 +75,8 @@ storiesOf("Components/Organisms/Chat", module)
                 attachments: [
                     {
                         name: "dummy.pdf",
-                        path: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                        path:
+                            "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
                     }
                 ]
             },
