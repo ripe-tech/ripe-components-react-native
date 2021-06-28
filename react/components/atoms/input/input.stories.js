4e5d50eee604bfe6624ff598c9109f3800f47746
Mauro Benedito <v-mb@platforme.com>
2021-06-28 16:32:23 +0100

    refactor: cleanup

--- react/components/atoms/input/input.stories.js
+++ react/components/atoms/input/input.stories.js
@@ -8,16 +8,12 @@ storiesOf("Components/Atoms/Input", module)
     .addDecorator(withKnobs)
     .add("Input", () => {
         const value = text("Value", undefined);
-        const showBorder = boolean("Show Border", true);
-        const borderColor = text("Border Color", "#e4e8f0");
         const placeholder = text("Placeholder", "Input value here");
         const height = number("Height", -1);
 
         return (
             <Input
                 value={value}
-                showBorder={showBorder}
-                borderColor={borderColor}
                 placeholder={placeholder}
                 height={height === -1 ? undefined : height}
             />
