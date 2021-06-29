a2d48111edb6447a149f20ba5f38c2480a4400cf
Margarida Silva <ms@platforme.com>
2021-06-29 18:14:02 +0100

    fix: remove styling

--- react/components/organisms/tabs-swipeable/tabs-swipeable.js
+++ react/components/organisms/tabs-swipeable/tabs-swipeable.js
@@ -244,8 +244,7 @@ const styles = StyleSheet.create({
     tabContent: {
         position: "absolute",
         width: "100%",
-        height: "100%",
-        backgroundColor: "red",
+        height: "100%"
     }
 });
 
