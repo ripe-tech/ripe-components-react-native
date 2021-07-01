014d6109be4e824f612f017e5f9f6bd83ea22ef3
Margarida Silva <ms@platforme.com>
2021-07-01 14:25:37 +0100

    fix: prop naming

--- react/components/contexts/onesignal-context/onesignal-context.js
+++ react/components/contexts/onesignal-context/onesignal-context.js
@@ -33,7 +33,7 @@ export class OneSignalProvider extends Component {
         try {
             await this.initOneSignal();
         } catch (err) {
-            this.props.handleErrorSimple(err);
+            this.props.errorHandler(err);
         }
     }
 
