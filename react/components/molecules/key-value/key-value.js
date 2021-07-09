1afd1386e330db60dc09d0c5ab15f84051e65c4b
3rdvision <kazgru@gmail.com>
2021-07-09 14:06:32 +0100

    feat: add prop to copy to clipboard on long press

--- react/components/molecules/key-value/key-value.js
+++ react/components/molecules/key-value/key-value.js
@@ -3,7 +3,7 @@ import { Platform, StyleSheet, Text, View, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 import { mix } from "yonius";
 
-import { IdentifiableMixin, baseStyles } from "../../../util";
+import { IdentifiableMixin, baseStyles, toClipboard } from "../../../util";
 
 import { ButtonIcon, Touchable } from "../../atoms";
 
@@ -23,6 +23,7 @@ export class KeyValue extends mix(PureComponent).with(IdentifiableMixin) {
             iconWidth: PropTypes.number,
             iconStrokeWidth: PropTypes.number,
             pressable: PropTypes.bool,
+            longPressToClipboard: PropTypes.bool,
             onPress: PropTypes.func,
             onButtonIconPress: PropTypes.func,
             onLongPress: PropTypes.func,
@@ -43,6 +44,7 @@ export class KeyValue extends mix(PureComponent).with(IdentifiableMixin) {
             iconWidth: undefined,
             iconStrokeWidth: undefined,
             pressable: false,
+            longPressToClipboard: false,
             onPress: () => {},
             onButtonIconPress: () => {},
             onLongPress: () => {},
@@ -69,6 +71,15 @@ export class KeyValue extends mix(PureComponent).with(IdentifiableMixin) {
         }
     };
 
+    _onPress = () => {
+        this.props.onPress(this.props._key, this.props.value);
+    };
+
+    _onLongPress = () => {
+        if (this.props.longPressToClipboard) toClipboard(this.props.value);
+        this.props.onLongPress(this.props._key, this.props.value);
+    };
+
     _style = () => {
         return [styles.keyValue, this._borderStyle(), this.props.style];
     };
@@ -78,8 +89,8 @@ export class KeyValue extends mix(PureComponent).with(IdentifiableMixin) {
             <Touchable
                 style={this._style()}
                 disabled={!this.props.pressable}
-                onPress={() => this.props.onPress(this.props._key, this.props.value)}
-                onLongPress={() => this.props.onLongPress(this.props._key, this.props.value)}
+                onPress={this._onPress}
+                onLongPress={this._onLongPress}
             >
                 <View style={styles.textContainer} {...this.id("key-value")}>
                     <Text style={this._keyStyle()} {...this.id("key-value-key")}>
