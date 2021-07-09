a750d400af09736be017df7199ac2e62cb15b35d
3rdvision <kazgru@gmail.com>
2021-07-09 10:12:56 +0100

    refactor: using same top distance between platforms after Input refactor

--- react/components/molecules/search/search.js
+++ react/components/molecules/search/search.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { StyleSheet, View, ViewPropTypes, Platform } from "react-native";
+import { StyleSheet, View, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 
 import { ButtonIcon, Input } from "../../atoms";
@@ -135,7 +135,7 @@ const styles = StyleSheet.create({
     button: {
         position: "absolute",
         backgroundColor: "transparent",
-        top: Platform.OS === "ios" ? 8 : 13,
+        top: 8,
         right: 18
     }
 });
