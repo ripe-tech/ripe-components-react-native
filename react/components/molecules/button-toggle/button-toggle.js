0a1599e66a0fb3c5ced87a9d181a9e8f3a041527
Margarida Silva <ms@platforme.com>
2021-07-13 10:51:42 +0100

    feat: butotn toggle flat variant and vertical orientation

--- react/components/molecules/button-toggle/button-toggle.js
+++ react/components/molecules/button-toggle/button-toggle.js
@@ -3,7 +3,7 @@ import { StyleSheet, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 import { mix } from "yonius";
 
-import { IdentifiableMixin } from "../../../util";
+import { IdentifiableMixin, capitalize } from "../../../util";
 
 import { Button } from "../../atoms";
 
@@ -16,7 +16,10 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
             color: PropTypes.string,
             colorSecondary: PropTypes.string,
             value: PropTypes.bool,
-            orientation: PropTypes.string,
+            callback: PropTypes.func,
+            variant: PropTypes.string,
+            direction: PropTypes.string,
+            toggle: PropTypes.bool,
             buttonProps: PropTypes.object,
             onUpdateActive: PropTypes.func,
             style: ViewPropTypes.style
@@ -31,7 +34,10 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
             color: "#f4f5f7",
             colorSecondary: "#4a6fe9",
             value: false,
-            orientation: undefined,
+            callback: undefined,
+            variant: undefined,
+            direction: undefined,
+            toggle: true,
             buttonProps: {},
             onUpdateActive: value => {},
             style: {}
@@ -42,7 +48,7 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
         super(props);
 
         this.state = {
-            valueData: props.value
+            valueData: props.toggle ? props.value : undefined
         };
     }
 
@@ -55,6 +61,9 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
     }
 
     onPress = event => {
+        if (this.props.callback) this.props.callback(event);
+        if (!this.props.toggle) return;
+
         this.setState(
             prevState => ({
                 valueData: !prevState.valueData
@@ -69,15 +78,19 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
         return this.props.icon ? null : this.props.text;
     };
 
+    _styleName = name => {
+        if (!this.props.variant) return name;
+        return `${name}${capitalize(this.props.variant)}`;
+    };
+
     _icon = () => {
         return this.state.valueData ? this.props.icon : this.props.iconSecondary || this.props.icon;
     };
 
     _color = () => {
-        return (
-            (this.state.valueData ? this.props.colorSecondary : this.props.color) ||
-            this.props.color
-        );
+        if (this.state.valueData) return this.props.colorSecondary;
+        if (this.props.variant) return;
+        return this.props.color;
     };
 
     _contentColor = () => {
@@ -87,24 +100,37 @@ export class ButtonToggle extends mix(PureComponent).with(IdentifiableMixin) {
     };
 
     _style = () => {
+        return [styles.buttonToggle];
+    };
+
+    _containerStyle = () => {
         return [
             styles.buttonToggle,
-            this.props.orientation === "middle" ? styles.buttonToggleMiddle : {},
-            this.props.orientation === "left" ? styles.buttonToggleLeft : {},
-            this.props.orientation === "right" ? styles.buttonToggleRight : {}
+            this.props.direction === "middle-horizontal"
+                ? styles[this._styleName("buttonToggleMiddleHorizontal")]
+                : {},
+            this.props.direction === "middle-vertical"
+                ? styles[this._styleName("buttonToggleMiddleVertical")]
+                : {},
+            this.props.direction === "left" ? styles[this._styleName("buttonToggleLeft")] : {},
+            this.props.direction === "right" ? styles[this._styleName("buttonToggleRight")] : {},
+            this.props.direction === "top" ? styles[this._styleName("buttonToggleTop")] : {},
+            this.props.direction === "bottom" ? styles[this._styleName("buttonToggleBottom")] : {}
         ];
     };
 
     render() {
         return (
             <Button
-                style={this._style()}
+                style={styles.buttonToggle}
+                containerStyle={this._containerStyle()}
                 text={this._text()}
                 icon={this._icon()}
                 backgroundColor={this._color()}
                 iconColor={this._contentColor()}
                 iconFillColor={this._contentColor()}
                 textColor={this._contentColor()}
+                variant={this.props.variant}
                 {...this.props.buttonProps}
                 onPress={this.onPress}
                 {...this.id("toggle-button")}
@@ -117,20 +143,63 @@ const styles = StyleSheet.create({
     buttonToggle: {
         overflow: "hidden",
         minWidth: 60,
-        borderRadius: 5
+        borderRadius: 0
     },
     buttonToggleLeft: {
         borderRadius: 0,
         borderTopLeftRadius: 5,
         borderBottomLeftRadius: 5
     },
-    buttonToggleMiddle: {
+    buttonToggleLeftFlat: {
+        borderRadius: 0,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    buttonToggleMiddleHorizontal: {
         borderRadius: 0
+    },
+    buttonToggleMiddleHorizontalFlat: {
+        borderRadius: 0,
+        borderLeftWidth: 0
+    },
+    buttonToggleMiddleVertical: {
+        borderRadius: 0
+    },
+    buttonToggleMiddleVerticalFlat: {
+        borderRadius: 0,
+        borderTopWidth: 0
     },
     buttonToggleRight: {
         borderRadius: 0,
         borderTopRightRadius: 5,
         borderBottomRightRadius: 5
+    },
+    buttonToggleRightFlat: {
+        borderRadius: 0,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+        borderLeftWidth: 0
+    },
+    buttonToggleTop: {
+        borderRadius: 0,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5
+    },
+    buttonToggleTopFlat: {
+        borderRadius: 0,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5
+    },
+    buttonToggleBottom: {
+        borderRadius: 0,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5
+    },
+    buttonToggleBottomFlat: {
+        borderRadius: 0,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        borderTopWidth: 0
     }
 });
 
