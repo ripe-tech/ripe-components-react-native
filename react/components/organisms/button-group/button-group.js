3acab7c6d0648e5611fd7b24cc322a122eccbb94
Margarida Silva <ms@platforme.com>
2021-07-13 11:56:43 +0100

    fix: change item callback to onPress

--- react/components/organisms/button-group/button-group.js
+++ react/components/organisms/button-group/button-group.js
@@ -104,7 +104,7 @@ export class ButtonGroup extends mix(PureComponent).with(IdentifiableMixin) {
                 direction={this._direction(index)}
                 align={this.props.align}
                 toggle={this.props.toggle}
-                onPress={item.callback}
+                onPress={item.onPress}
                 onUpdateActive={() => this.onUpdateActive(item.value)}
                 {...item.buttonProps}
             />
