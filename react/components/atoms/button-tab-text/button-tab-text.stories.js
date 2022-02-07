0a907b3f89aa7bbb2e97625ab52416f416512ce5
Margarida Silva <ms@platforme.com>
2022-02-07 14:35:39 +0000

    feat: update tabs and button tab stories

--- react/components/atoms/button-tab-text/button-tab-text.stories.js
+++ react/components/atoms/button-tab-text/button-tab-text.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs, boolean, select, text } from "@storybook/addon-knobs";
+import { withKnobs, boolean, text } from "@storybook/addon-knobs";
 
 import { ButtonTabText } from "./button-tab-text";
 
@@ -8,24 +8,22 @@ storiesOf("Atoms", module)
     .addDecorator(withKnobs)
     .add("Button Tab Text", () => {
         const _text = text("Text", "User");
-        const backgroundColor = text("Background Color", null);
+        const backgroundColor = text("Background Color", undefined);
+        const backgroundColorSelected = text("Selected Background Color", undefined);
+        const color = text("Color", undefined);
+        const colorSelected = text("Selected Color", undefined);
         const disabled = boolean("Disabled", false);
         const active = boolean("Active", true);
-        const variant = select(
-            "Variant",
-            {
-                Unset: undefined,
-                Compact: "compact"
-            },
-            undefined
-        );
+
         return (
             <ButtonTabText
                 active={active}
                 text={_text || undefined}
                 disabled={disabled}
                 backgroundColor={backgroundColor}
-                variant={variant}
+                backgroundColorSelected={backgroundColorSelected}
+                color={color}
+                colorSelected={colorSelected}
                 onPress={() => alert("Thanks for the press!")}
             />
         );
--- react/components/molecules/tabs-text/tabs-text.stories.js
+++ react/components/molecules/tabs-text/tabs-text.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react-native";
-import { withKnobs, select } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
 import { TabsText } from "./tabs-text";
 
@@ -14,16 +14,22 @@ storiesOf("Molecules", module)
             { text: "New", disabled: false },
             { text: "Past", disabled: true }
         ];
-        const variant = select(
-            "Variant",
-            {
-                Unset: undefined,
-                Compact: "compact"
-            },
-            undefined
-        );
+        const tabsBackgroundColor = text("Background Color", undefined);
+        const tabsBackgroundColorSelected = text("Selected Background Color", undefined);
+        const tabsColor = text("Tab Text Color", undefined);
+        const tabsColorSelected = text("Selected Tab Text Color", undefined);
 
         const onTabChange = tabIndex => alert(`Switched to index: ${tabIndex}`);
 
-        return <TabsText tabs={tabs} tabSelected={0} variant={variant} onTabChange={onTabChange} />;
+        return (
+            <TabsText
+                tabs={tabs}
+                tabSelected={0}
+                tabsBackgroundColor={tabsBackgroundColor}
+                tabsBackgroundColorSelected={tabsBackgroundColorSelected}
+                tabsColor={tabsColor}
+                tabsColorSelected={tabsColorSelected}
+                onTabChange={onTabChange}
+            />
+        );
     });
