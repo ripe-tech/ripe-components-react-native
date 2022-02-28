eb1cd4da78181c967224bf87b2db7c05824bf46f
Margarida Silva <ms@platforme.com>
2022-02-28 16:45:46 +0000

    refactor: remove unused dependency

--- react/components/atoms/image-loading/image-loading.js
+++ react/components/atoms/image-loading/image-loading.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { Image, Platform, StyleSheet, View, ViewPropTypes } from "react-native";
+import { Image, StyleSheet, View, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 
 import { equal } from "yonius";
