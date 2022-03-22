0a6e51ad5bf0e306a7f16487fe6cf18c9c61bc06
3rdvision <kazgru@gmail.com>
2022-03-22 17:49:06 +0000

    Revert "fix: image-loading flicker"
    This reverts commit 7b03600e73e0514189cea583e4eb19382e1fcd56.

--- react/components/atoms/image-loading/image-loading.js
+++ react/components/atoms/image-loading/image-loading.js
@@ -54,7 +54,7 @@ export class ImageLoading extends PureComponent {
     async componentDidUpdate(prevProps) {
         if (
             prevProps.uri !== this.props.uri ||
-            prevProps.src !== this.props.src ||
+            prevProps.src !== this.props.uri ||
             !equal(prevProps.source, this.props.source)
         ) {
             await this._verifyCache();
