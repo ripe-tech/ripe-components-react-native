2946a1a8bb12e107f8ced66b3595484f3da34558
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-03-29 16:15:24 +0100

    fix: remove unused import

--- react/components/molecules/search/search.js
+++ react/components/molecules/search/search.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { Animated, StyleSheet, View, ViewPropTypes } from "react-native";
+import { Animated, StyleSheet, ViewPropTypes } from "react-native";
 import PropTypes from "prop-types";
 
 import { baseStyles } from "../../../util";
